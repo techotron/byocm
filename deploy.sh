82e8f6deaf43424fcc5b5095faaa341241b8daa8
Eddy Snow <eddysnow@gmail.com>
2019-03-19 21:04:43 +0000

    add wait commands for vpc stack to finishing creating or updating

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,9 @@ echo "[$(date)] - Deploying stacks to region: $AWS_REGION"
 if [ ! $(aws cloudformation describe-stacks --region $AWS_REGION --profile $AWS_PROFILE | jq '.Stacks[].StackName' | grep $VPC_STACK_NAME) ]; then
     echo "[$(date)] - Creating $VPC_STACK_NAME stack"
     aws cloudformation create-stack --stack-name $VPC_STACK_NAME --template-body file://./infrastructure/vpc.yml --profile $AWS_PROFILE --region $AWS_REGION
+    aws cloudformation wait stack-create-complete --stack-name $VPC_STACK_NAME --profile $AWS_PROFILE --region $AWS_REGION
 else
     echo "[$(date)] - Updating $VPC_STACK_NAME stack"
     aws cloudformation update-stack --stack-name $VPC_STACK_NAME --template-body file://./infrastructure/vpc.yml --profile $AWS_PROFILE --region $AWS_REGION
+    aws cloudformation wait stack-update-complete --stack-name $VPC_STACK_NAME --profile $AWS_PROFILE --region $AWS_REGION
 fi
-
