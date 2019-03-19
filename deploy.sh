9458a12982ad50194c86588e258f80c1b012d53c
Eddy Snow <eddysnow@gmail.com>
2019-03-19 23:10:33 +0000

    fix zip command to retain child folder structure and include cfn template parameter to package archive

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 AWS_REGION="eu-west-1"
 AWS_PROFILE="intapp-devopssbx_eddy.snow@intapp.com"
-APP_VERSION="$(date +%F_%H%M%S)"
+PACKAGE_VERSION="$(date +%F_%H%M%S)"
+PACKAGE_URL="https://s3-eu-west-1.amazonaws.com/278942993584-eddy-scratch/git/byocm/wordpress/$(echo $PACKAGE_VERSION)_Package.zip"
 
 EC2_KEY_NAME="eddy.snow@intapp-devopssbx"
 DOCKER_AMI="ami-07683a44e80cd32c5"
@@ -13,11 +14,10 @@ DOCKER_STACK_NAME="byocm-docker"
 echo "[$(date)] - Packaging wordpress solution"
 
 # Package Python functions
-zip -j /tmp/$(echo $APP_VERSION)_appPackage.zip ./wordpress/*
+zip -r /tmp/$(echo $PACKAGE_VERSION)_Package.zip ./wordpress/*
 
 # Upload package to s3
-aws s3 cp /tmp/$(echo $APP_VERSION)_appPackage.zip s3://278942993584-eddy-scratch/git/byocm/wordpress/ --profile $AWS_PROFILE
-
+aws s3 cp /tmp/$(echo $PACKAGE_VERSION)_Package.zip s3://278942993584-eddy-scratch/git/byocm/wordpress/ --profile $AWS_PROFILE
 
 # Deploy AWS Infrastructure
 echo "[$(date)] - Deploying stacks to region: $AWS_REGION"
@@ -39,6 +39,7 @@ if [ ! $(aws cloudformation describe-stacks --region $AWS_REGION --profile $AWS_
         --parameters \
             ParameterKey=keyName,ParameterValue=$EC2_KEY_NAME \
             ParameterKey=ec2Image,ParameterValue=$DOCKER_AMI \
+            ParameterKey=dockerPackageName,ParameterValue=$PACKAGE_URL \
         --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
     aws cloudformation wait stack-create-complete --stack-name $DOCKER_STACK_NAME --profile $AWS_PROFILE --region $AWS_REGION
 else
@@ -47,6 +48,7 @@ else
         --parameters \
             ParameterKey=keyName,ParameterValue=$EC2_KEY_NAME \
             ParameterKey=ec2Image,ParameterValue=$DOCKER_AMI \
+            ParameterKey=dockerPackageName,ParameterValue=$PACKAGE_URL \
         --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
     aws cloudformation wait stack-update-complete --stack-name $DOCKER_STACK_NAME --profile $AWS_PROFILE --region $AWS_REGION
 fi
